234051d90a8fd7ecc2c076b338c94b91f4bcf5c3
flavionasc <flavio_radioativa@hotmail.com>
2016-11-03 17:27:06 -0200

    add casos de teste

--- test/FactorialServiceSpec.js
+++ test/FactorialServiceSpec.js
@@ -1,7 +1,15 @@
 describe('FactorialService.js', function() {
 
     it('shold calculate the Factorial from given number', function(){
+        expect(FactorialService.calculate(1)).toBe(1);
+        expect(FactorialService.calculate(2)).toBe(2);
+        expect(FactorialService.calculate(3)).toBe(6);
+        expect(FactorialService.calculate(4)).toBe(24);
         expect(FactorialService.calculate(5)).toBe(120);
+    });
+
+    it('shold return 1 (one) if the given number is 0 (zero)', function(){
+        expect(FactoryService.calculate(0)).toBe(1);
     });
 
 });
